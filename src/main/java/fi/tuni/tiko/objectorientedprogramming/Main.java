ae967761411bc5c2ebc2d4f0363cbe9b5c42bcff
Tiina Malinen <malitsu@gmail.com>
2019-12-18 10:12:22 +0200

    Add scrollbar if list grows large. Tweak the parsing calling according to changes in the Parser side. Create separate Main-class.

---
+++ src/main/java/fi/tuni/tiko/objectorientedprogramming/Main.java
@@ -0,0 +1,9 @@
+package fi.tuni.tiko.objectorientedprogramming;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Shoppinglist.class, args);
+    }
+}
